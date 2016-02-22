c89a470b22cce673667ce06dd34188bf317c44c1
Colm Laro <smartman97@gmail.com>
2016-02-22 09:30:17 -0700

    Added all methods in .cpp

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -20,8 +20,67 @@ CTECList<Type>::~CTECList()
 }
 
 template<class Type>
-int CTECArray<Type>::getSize()
+int CTECList<Type>::getSize()
 {
 	return this->size;
 }
 
+template<class Type>
+Type CTECList<Type>::getFront()
+{
+	return nullptr;
+}
+
+template<class Type>
+Type CTECList<Type>::getEnd()
+{
+	return nullptr;
+}
+
+template<class Type>
+Type CTECList<Type>::getAtIndex(int index)
+{
+	return nullptr;
+}
+
+template<class Type>
+void CTECList<Type>::set(int index, const Type& value)
+{
+
+}
+
+template<class Type>
+void CTECList<Type>::addFront(const Type& value)
+{
+
+}
+
+template<class Type>
+void CTECList<Type>::addEnd(const Type& value)
+{
+
+}
+
+template<class Type>
+void CTECList<Type>::addAtIndex(int index, const Type& value)
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::removeFront()
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::removeEnd()
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::removeAtIndex(int index)
+{
+
+}
