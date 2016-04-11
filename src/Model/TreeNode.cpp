68a99a900a4151aace98c418e6f51adf97b663af
Colm Laro <smartman97@gmail.com>
2016-04-11 08:20:25 -0600

    Adding the TreeNode class

---
+++ src/Model/TreeNode.cpp
@@ -0,0 +1,21 @@
+/*
+ * TreeNode.cpp
+ *
+ *  Created on: Apr 11, 2016
+ *      Author: clar5254
+ */
+
+#include "TreeNode.h"
+
+template<class Type>
+TreeNode::TreeNode()
+{
+
+}
+
+template<class Type>
+TreeNode::~TreeNode()
+{
+
+}
+
---
+++ src/Model/TreeNode.h
@@ -0,0 +1,34 @@
+/*
+ * TreeNode.h
+ *
+ *  Created on: Apr 11, 2016
+ *      Author: clar5254
+ */
+
+#ifndef MODEL_TREENODE_H_
+#define MODEL_TREENODE_H_
+
+#include "Node.h"
+
+template<class Type>
+class TreeNode : public Node<Type>
+{
+public:
+	TreeNode();
+	TreeNode(const Type& value);
+	TreeNode(const Type& value, TreeNode<Type>* parent);
+	void setLeftChild(TreeNode<Type> leftChild);
+	void setRightChild(TreeNode<Type> rightChild);
+	void setParent(TreeNode<Type> parent);
+	TreeNode<Type>* getLeftChild();
+	TreeNode<Type>* getRightChild();
+	TreeNode<Type>* getParent();
+	virtual ~TreeNode();
+
+private:
+	TreeNode<Type>* leftChild;
+	TreeNode<Type>* rightChild;
+	TreeNode<Type>* parent;
+};
+
+#endif /* MODEL_TREENODE_H_ */
