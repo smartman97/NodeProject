81678797cf60622e827cd3da2d5c4144e6e3ed5b
Colm Laro <smartman97@gmail.com>
2016-04-25 08:54:05 -0600

    Added framework and methods

--- src/Model/Graph.cpp
+++ src/Model/Graph.cpp
@@ -7,13 +7,93 @@
 
 #include "Graph.h"
 
-Graph::Graph()
+template<class Type>
+const int Graph<Type>::MAXIMUM;
+
+template<class Type>
+Graph<Type>::Graph()
 {
 
 }
 
-Graph::~Graph()
+template<class Type>
+Graph<Type>::~Graph()
 {
 
 }
 
+template<class Type>
+void Graph<Type>::addEdge(int source, int target)
+{
+	assert(source < size());
+	assert(target < size());
+	edges[source][target] = true;
+}
+
+template<class Type>
+void Graph<Type>::addVertex(const Type& vertexLabel)
+{
+	int newVertexNumber;
+	int otherNumber;
+
+	assert(size() < MAXIMUM);
+
+	newVertexNumber = manyVertices;
+	++manyVertices;
+
+	for(otherNumber = 0; otherNumber < manyVertices; otherNumber++)
+	{
+		edges[otherNumber][newVertexNumber] = false;
+		edges[newVertexNumber][otherNumber] = false;
+	}
+
+	vertexLabel[newVertexNumber] = vertexLabel;
+}
+
+template<class Type>
+bool Graph<Type>::isEdge(int source, int target) const
+{
+	bool connected = false;
+
+	assert(source < size() && target < size());
+	connected = edges[source][target];
+
+	return connected;
+}
+
+template<class Type>
+Type& Graph<Type>::operator [](int vertex)
+{
+	assert(vertex < size());
+	return labels[vertex];
+}
+
+template<class Type>
+Type Graph<Type>::operator [](int vertex) const
+{
+	assert(vertex < size());
+	return labels[vertex];
+}
+
+template<class Type>
+std::set<int> Graph<Type>::neighbors(int vertex) const
+{
+	std::set<int> answer;
+	assert(vertex < size());
+
+	for(int index = 0; index < size(); index++)
+	{
+		if(edges[vertex][index])
+		{
+			answer.insert(index);
+		}
+	}
+	return answer;
+}
+
+template<class Type>
+void Graph<Type>::removeEdge(int source, int target)
+{
+	assert(source < size() && target < size());
+	edges[source][target] = false;
+}
--- src/Model/Graph.h
+++ src/Model/Graph.h
@@ -7,20 +7,29 @@
 
 #ifndef MODEL_GRAPH_H_
 #define MODEL_GRAPH_H_
+#include <set>
 
 template<class Type>
 class Graph
 {
 private:
 	static const int MAXIMUM = 20;
+	bool edges [MAXIMUM][MAXIMUM];
+	Type labels [MAXIMUM];
+	int manyVertices;
 public:
 	Graph();
 	virtual ~Graph();
 
-	void addVertex(const Type& label);
+	void addVertex(const Type& vertexLabel);
 	void addEdge(int source, int target);
 	void removeEdge(int source, int target);
 	Type& operator [] (int vertex);
+
+	int size() const;
+	bool isEdge(int source, int target) const;
+	std::set<int> neighbors(int vertex) const;
+	Type operator [] (int vertex) const;
 };
 
 #endif /* MODEL_GRAPH_H_ */
